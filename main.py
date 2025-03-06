3d625b0d91817c4f231382e9e2e7dd0a26841487
LimafBeila2 <golbolidary002@gmail.com>
2025-03-06 21:09:17 +0400

    чячяЯЧяЧ

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import json
 import threading
 import queue
@@ -32,6 +31,7 @@ def create_driver():
     options.add_argument("--window-size=1920x1080")
     service = Service("/usr/bin/chromedriver")
     return webdriver.Chrome(service=service, options=options)
+
 
 # Функция входа в Umico Business
 def login_to_umico(driver):
@@ -86,7 +86,18 @@ def process_product(q):
             driver.get(product_url)
             sleep(2)
             close_ad(driver)
-
+            
+            try:
+                button = WebDriverWait(driver, 30).until(
+                    EC.element_to_be_clickable((By.XPATH,
+                        "//a[contains(text(), 'Посмотреть цены всех продавцов') or contains(text(), 'Bütün satıcıların qiymətlərinə baxmaq')]"
+                    ))
+                )
+                button.click()
+            except:
+                logging.warning("Не удалось найти кнопку просмотра цен.")
+                continue
+            
             WebDriverWait(driver, 30).until(
                 EC.presence_of_all_elements_located((By.CLASS_NAME, "MPProductOffer"))
             )
@@ -95,30 +106,23 @@ def process_product(q):
             if not product_offers:
                 logging.warning("Нет предложений по этому товару.")
                 continue
-
+            
             lowest_price = float('inf')
             lowest_price_merchant = ""
             super_store_price = None
             
-            # Извлечение предложений других продавцов
-            other_sellers_section = driver.find_elements(By.CLASS_NAME, "MPProductPricesOtherSellers")
-            if not other_sellers_section:
-                logging.warning("Не удалось найти блок с предложениями от других продавцов.")
-                continue
-
-            for seller in other_sellers_section:
+            for offer in product_offers:
                 try:
-                    # Найдем имя продавца
-                    merchant = seller.find_element(By.CLASS_NAME, "NameMerchant").text.strip()
-
-                    # Получим старую и новую цену
-                    price_text_old = seller.find_element(By.XPATH, ".//span[@data-info='item-desc-price-old']").text.strip() if seller.find_elements(By.XPATH, ".//span[@data-info='item-desc-price-old']") else None
-                    price_text_new = seller.find_element(By.XPATH, ".//span[@data-info='item-desc-price-new']").text.strip() if seller.find_elements(By.XPATH, ".//span[@data-info='item-desc-price-new']") else None
-
+                    merchant = offer.find_element(By.CLASS_NAME, "NameMerchant").text.strip()
+                    
+                    price_text_old = offer.find_element(By.XPATH, ".//span[@data-info='item-desc-price-old']").text.strip() if offer.find_elements(By.XPATH, ".//span[@data-info='item-desc-price-old']") else None
+                    price_text_new = offer.find_element(By.XPATH, ".//span[@data-info='item-desc-price-new']").text.strip() if offer.find_elements(By.XPATH, ".//span[@data-info='item-desc-price-new']") else None
+                    
                     # Выбираем минимальную цену, если оба атрибута найдены
                     price_text = None
                     if price_text_old and price_text_new:
                         price_text = min(price_text_old, price_text_new, key=lambda x: float(x.replace("₼", "").replace(" ", "").strip()))  # Убираем пробелы
+
 
                     elif price_text_old:
                         price_text = price_text_old
@@ -139,15 +143,15 @@ def process_product(q):
                 except Exception as e:
                     logging.warning(f"Ошибка при обработке предложения: {e}")
                     continue
-
+            
             logging.info(f"Самая низкая цена: {lowest_price} от {lowest_price_merchant}")
             if super_store_price is not None:
                 logging.info(f"Цена от Super Store: {super_store_price}")
-            
+
             if lowest_price <= 80:
                 logging.info(f"Самая низкая цена ({lowest_price}₼) слишком низкая, пропускаем товар.")
                 continue
-
+            
             if super_store_price is not None and lowest_price < super_store_price:
                 logging.info("Меняем цену...")
                 driver.get(edit_url)
@@ -191,26 +195,24 @@ def process_products_from_json(json_file):
     products = load_json(json_file)
     q = queue.Queue()
 
-    while True:  # Бесконечный цикл
-        # Добавляем все товары обратно в очередь
-        for product in products:
-            q.put(product)
+    for product in products:
+        q.put(product)
 
-        threads = []
-        num_threads = min(3, len(products))  # Запускаем не больше 10 потоков
+    threads = []
+    num_threads = min(3, len(products))  # Запускаем не больше 10 потоков
 
-        for _ in range(num_threads):
-            thread = threading.Thread(target=process_product, args=(q,))
-            threads.append(thread)
-            thread.start()
+    for _ in range(num_threads):
+        thread = threading.Thread(target=process_product, args=(q,))
+        threads.append(thread)
+        thread.start()
 
-        q.join()
+    q.join()
 
-        for thread in threads:
-            thread.join()
-
-        logging.info("Цикл завершен, начинаем заново...")
+    for thread in threads:
+        thread.join()
 
 if __name__ == "__main__":
-    process_products_from_json("product.json")
-    logging.info("Работа завершена!")
+    while True:
+        process_products_from_json("product.json")
+        logging.info("Работа завершена! Перезапуск через 1 секунду...")
+        sleep(1)
